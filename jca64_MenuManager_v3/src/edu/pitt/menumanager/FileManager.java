da5f3ef81ea76937d57511dc75dc47502c9a5374
jca64 <jca64@pitt.edu>
2022-11-27 22:21:30 -0500

    Assignment 3

--- jca64_MenuManager_v3/src/edu/pitt/menumanager/FileManager.java
+++ jca64_MenuManager_v3/src/edu/pitt/menumanager/FileManager.java
@@ -2,8 +2,10 @@ package edu.pitt.menumanager;
 
 import java.util.ArrayList;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 
 /**
  * Class Entree
@@ -12,111 +14,73 @@ import java.io.BufferedReader;
  */
 
 public class FileManager {
-	
-	/**
-	* Method readEntrees
-	* @param fileName Name of txt file that holds entrees
-	* @return An ArrayList of entrees
-	*/
 
-	public static ArrayList<Entree> readEntrees(String fileName) {
-		ArrayList<Entree> en = new ArrayList<>();
+	// reads items from file provided and puts them into an ArrayList with the correct type
+	public static ArrayList<MenuItem> readItems(String fileName) {
+		ArrayList<MenuItem> items = new ArrayList<>();
+
 		try {
 			FileReader fr = new FileReader(fileName);
 			BufferedReader br = new BufferedReader(fr);
 			String line = null;
 			while((line = br.readLine()) != null) {
-				String[] lineSplt = line.split("@@");
-				Entree e = new Entree(lineSplt[0], lineSplt[1], Integer.parseInt(lineSplt[2]));
-				en.add(e);
+				String[] lineSplit = line.split("@@");
+
+				switch(lineSplit[1]) {
+				case "entree":
+					Entree e = new Entree(lineSplit[0], lineSplit[2], Integer.parseInt(lineSplit[3]), Double.parseDouble(lineSplit[4]));
+					items.add(e);
+					break;
+				case "side":
+					Side si = new Side(lineSplit[0], lineSplit[2], Integer.parseInt(lineSplit[3]), Double.parseDouble(lineSplit[4]));
+					items.add(si);
+					break;
+				case "salad":
+					Salad sa = new Salad(lineSplit[0], lineSplit[2], Integer.parseInt(lineSplit[3]), Double.parseDouble(lineSplit[4]));
+					items.add(sa);
+					break;
+				case "dessert":
+					Dessert d = new Dessert(lineSplit[0], lineSplit[2], Integer.parseInt(lineSplit[3]), Double.parseDouble(lineSplit[4]));
+					items.add(d);
+					break;
+				}
 			}
 			fr.close();
 			br.close();
 		} catch(IOException e) {
-			System.out.print(e);
 			System.out.print("File does not exist.");
-		} finally {
-			
-		}
-		return en;
+		} 
+		return items;
 	}
-	
-	/**
-	* Method readSides
-	* @param fileName Name of txt file that holds sides
-	* @return An ArrayList of sides
-	*/
-	public static ArrayList<Side> readSides(String fileName) {
-		ArrayList<Side> si = new ArrayList<>();
+
+	// writes menus to file. separates different menus by "@@" and elements of the menu with " / "
+	public static void writeMenus(String fileName, ArrayList<Menu> menus) {
+
 		try {
-			FileReader fr = new FileReader(fileName);
-			BufferedReader br = new BufferedReader(fr);
-			String line = null;
-			while((line = br.readLine()) != null) {
-				String[] lineSplt = line.split("@@");
-				Side s = new Side(lineSplt[0], lineSplt[1], Integer.parseInt(lineSplt[2]));
-				si.add(s);
+			FileWriter fw = new FileWriter(fileName);
+			BufferedWriter bw = new BufferedWriter(fw);
+
+			for(Menu i : menus) {
+
+				Entree e = i.getEntree();
+				Side si = i.getSide();
+				Salad sa = i.getSalad();
+				Dessert d = i.getDessert();
+
+				int totalCal = e.getCalories() + si.getCalories() + sa.getCalories() + d.getCalories();
+				double totalPrice = e.getPrice() + si.getPrice() + sa.getPrice() + d.getPrice();
+
+				bw.write(i.getName() + "@@" + e + " / " + e.getDescription() + " / " + 
+						e.getCalories() + "@@" + si + " / " + si.getDescription() + " / " +
+						si.getCalories() + "@@" + sa + " / " + sa.getDescription() + " / " +
+						sa.getCalories() + "@@" + d + " / " + d.getDescription() + " / " +
+						d.getCalories() + "@@" + totalCal + "@@" + totalPrice + "\n");
 			}
-			fr.close();
-			br.close();
+			bw.close();
+			fw.close();
 		} catch(IOException e) {
-			System.out.print("File does not exist.");
-		} finally {
-			
+			System.out.println("File not found.");
 		}
-		return si;
-	}
-	
-	/**
-	* Method readSalads
-	* @param fileName Name of txt file that holds salads
-	* @return An ArrayList of salads
-	*/
-	public static ArrayList<Salad> readSalads(String fileName) {
-		ArrayList<Salad> sa = new ArrayList<>();
-		try {
-			FileReader fr = new FileReader(fileName);
-			BufferedReader br = new BufferedReader(fr);
-			String line = null;
-			while((line = br.readLine()) != null) {
-				String[] lineSplt = line.split("@@");
-				Salad s = new Salad(lineSplt[0], lineSplt[1], Integer.parseInt(lineSplt[2]));
-				sa.add(s);
-			}
-			fr.close();
-			br.close();
-		} catch(IOException e) {
-			System.out.print("File does not exist.");
-		} finally {
-			
-		}
-		return sa;
-	}
-	
-	/**
-	* Method readDesserts
-	* @param fileName Name of txt file that holds desserts
-	* @return An ArrayList of desserts
-	*/
-	public static ArrayList<Dessert> readDesserts(String fileName) {
-		ArrayList<Dessert> de = new ArrayList<>();
-		try {
-			FileReader fr = new FileReader(fileName);
-			BufferedReader br = new BufferedReader(fr);
-			String line = null;
-			while((line = br.readLine()) != null) {
-				String[] lineSplt = line.split("@@");
-				Dessert d = new Dessert(lineSplt[0], lineSplt[1], Integer.parseInt(lineSplt[2]));
-				de.add(d);
-			}
-			fr.close();
-			br.close();
-		} catch(IOException e) {
-			System.out.print("File does not exist.");
-		} finally {
-			
-		}
-		return de;
-	}
-	
+
+	}	
 }
